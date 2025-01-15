41ac27058eaa82e970bf9b80a2aea2fa8c8ce1e2
JoooheonPark <parjjh@naver.com>
2025-01-15 14:15:23 +0900

    #shopify : 기본 화면 구성

---
+++ resources/js/app.jsx
@@ -0,0 +1,16 @@
+import '@shopify/polaris/build/esm/styles.css';
+import { AppProvider, Button } from '@shopify/polaris';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <AppProvider i18n={{}}>
+            <Button onClick={() => window.location.href='mailto:support@quickbulkedit.com'}>
+                Customer Support
+            </Button>
+        </AppProvider>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
