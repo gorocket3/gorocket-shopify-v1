dc8284803af5f835f6f6b86faf7bd58059196e8d
JoooheonPark <parjjh@naver.com>
2025-03-04 15:55:11 +0900

    #shopify - 상품 로그 테이블 변경 및 수정 변경

--- database/migrations/2025_02_13_073212_create_change_logs_table.php
+++ database/migrations/2025_02_13_073212_create_change_logs_table.php
@@ -20,8 +20,6 @@ return new class extends Migration
             $table->json('old_data');
             $table->json('new_data');
             $table->timestamps();
-
-            $table->foreign('product_id')->references('product_id')->on('products')->onDelete('cascade');
         });
     }
 
