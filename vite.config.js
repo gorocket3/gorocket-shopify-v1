070df36876b397a5e52ed240b87dbb61703aaa36
uhyun-fe <feyouhyun0957@gmail.com>
2025-03-12 10:51:16 +0900

    #shopify - vite.config.js 수정 (input files 추가)

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,13 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/js/app.jsx',
-                'resources/js/plan/app.jsx',
-                'resources/js/product/app.jsx',
                 'resources/js/history/app.jsx',
-                'resources/css/app.css'
+                'resources/js/plan/app.jsx',
+                'resources/js/products/app.jsx',
+                'resources/js/products/columns.js',
+                'resources/js/products/grid_controller.js',
+                // 'resources/js/product/app.jsx',
+                'resources/css/app.css',
             ],
             refresh: true,
         }),
