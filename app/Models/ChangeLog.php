f3db88d0e41b944e140117d9685c3514118cbd6a
JoooheonPark <parjjh@naver.com>
2025-03-17 11:03:55 +0900

    #shopify - 상품수정 로그 API 추가

--- app/Models/ChangeLog.php
+++ app/Models/ChangeLog.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method static where(string $string, $id)
+ */
 class ChangeLog extends Model
 {
     /**
