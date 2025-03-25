45cd7ee7c3352ad2144d9379eefe3b11313315d2
uhyun-fe <feyouhyun0957@gmail.com>
2025-03-25 11:47:35 +0900

    #shopify - grid script tag 캐시처리

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -32,12 +32,12 @@
     <script>
         agGrid.LicenseManager.setLicenseKey("{{ config('services.grid.license') }}");
     </script>
-    <script src="/assets/grid/function.js?v=2025021017"></script>
-    <script src="/assets/grid/init.js?v=2025021017"></script>
-    <script src="/assets/grid/grid_field_editor.js?v=2025021017"></script>
+    <script src="/assets/grid/function.js?v=2025032511"></script>
+    <script src="/assets/grid/init.js?v=2025032511"></script>
+    <script src="/assets/grid/grid_field_editor.js?v=2025032511"></script>
     {{--    <script src="/assets/grid/grid_custom_editor.js?v=2025021017"></script>--}}
-    <script src="/assets/grid/grid.js?v=2025021017"></script>
-    <link rel="stylesheet" href="/assets/grid/grid.css?v=2025021017">
+    <script src="/assets/grid/grid.js?v=2025032511"></script>
+    <link rel="stylesheet" href="/assets/grid/grid.css?v=2025032511">
     <!-- // AG-GRID -->
 
     <!-- Font -->
