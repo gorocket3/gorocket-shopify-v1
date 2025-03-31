623e2bbc3c336a0b4cd8714e2a33bd86839b4d82
JoooheonPark <parjjh@naver.com>
2025-03-31 15:07:42 +0900

    #shopify - vite 모듈파일 청크

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
+import {defineConfig, loadEnv} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(function ({ mode }) {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -17,6 +17,21 @@ export default defineConfig(function ({ mode }) {
         },
         resolve: {
             preserveSymlinks: true,
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            if (id.includes('react')) return 'react';
+                            if (id.includes('@shopify')) return 'shopify';
+                            if (id.includes('ag-grid')) return 'grid';
+                            if (id.includes('axios')) return 'vendor';
+                            return 'vendor-other';
+                        }
+                    }
+                }
+            }
         },
         server: {
             host: "localhost",
