7a788be41875c77833f5a15e8f254f49f11a997e
uhyun-fe <feyouhyun0957@gmail.com>
2025-03-31 15:52:29 +0900

    #shopify - Plan confirmation url 링크 적용

--- resources/frontend/utils/hooks.js
+++ resources/frontend/utils/hooks.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { getPlanConfirmationUrl } from "./api";
+import { showError } from "./toasts";
 
 export function useEffectWithoutInitialState(callback, state) {
     const [ init, setInit ] = useState(true);
@@ -12,6 +14,23 @@ export function useEffectWithoutInitialState(callback, state) {
     }, state);
 }
 
-export function goToChargesPage(planId = 2) {
-    window.open(`/apps/${import.meta.env.VITE_APP_NAME}/billing/${planId}`, "_top");
+export async function goToChargesPage(planId = 2) {
+    try {
+        const { url } = await getPlanConfirmationUrl({ planId, host: getStorage('host') });
+        window.open(url, "_top");
+    } catch (e) {
+        showError('Failed to get confirmation URL. Please try again.');
+    }
+}
+
+export function initStorage() {
+    const host = new URLSearchParams(location.search).get("host");
+    const sessionValue = { host };
+    sessionStorage.setItem("shopify-gorocket-editor", JSON.stringify(sessionValue));
+}
+
+export function getStorage(key, type = 'session') {
+    const storage = type === 'session' ? sessionStorage : localStorage;
+    const info = JSON.parse(storage.getItem('shopify-gorocket-editor')) || {};
+    return info[key] || '';
 }
