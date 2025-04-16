16df7b76452a7a85da0d793f2058122cb46f2d56
JoooheonPark <parjjh@naver.com>
2025-04-16 12:11:57 +0900

    #shopify - 무료플랜 에디트 수 변경

--- config/plans.php
+++ config/plans.php
@@ -13,7 +13,7 @@ return [
     */
 
     'edit_limits' => [
-        'Free'  => env('PLAN_EDIT_LIMIT_FREE', 20),
+        'Free'  => env('PLAN_EDIT_LIMIT_FREE', 50),
         'Basic' => env('PLAN_EDIT_LIMIT_BASIC', 5000),
         'Pro' => env('PLAN_EDIT_LIMIT_PRO', 10000)
     ],
