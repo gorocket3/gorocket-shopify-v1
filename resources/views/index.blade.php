93804de6d3e0fd524ef898e45b23cc3fc4b84ba3
JoooheonPark <parjjh@naver.com>
2025-04-18 12:19:35 +0900

    #shopify - 서포트 스토어 자동입력

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,7 +55,16 @@
 
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const shopParam = urlParams.get('shop');
+        const shopName = shopParam ? shopParam.split('.')[0] : '';
+
+        Tawk_API.visitor = {
+            name: shopName
+        };
+
         (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
