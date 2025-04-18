827f29eb8f01776598dadc27e5cb0e8f6e021404
JoooheonPark <parjjh@naver.com>
2025-04-18 14:18:58 +0900

    #shopify - 서포트 스토어 변경시 처리

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,14 +57,6 @@
     <script type="text/javascript">
         var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 
-        const urlParams = new URLSearchParams(window.location.search);
-        const shopParam = urlParams.get('shop');
-        const shopName = shopParam ? shopParam.split('.')[0] : '';
-
-        Tawk_API.visitor = {
-            name: shopName
-        };
-
         (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
@@ -73,6 +65,17 @@
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
         })();
+
+        Tawk_API.onLoad = function () {
+            const urlParams = new URLSearchParams(window.location.search);
+            const shopParam = urlParams.get('shop');
+            const shopName = shopParam ? shopParam.split('.')[0] : '';
+            Tawk_API.setAttributes({
+                name: shopName
+            }, function (error) {
+                if (error) console.error("Tawk.to setAttributes error:", error);
+            });
+        };
     </script>
     <!--End of Tawk.to Script-->
 
