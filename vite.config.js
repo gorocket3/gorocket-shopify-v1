b9936456d8407bae64b67d35ccf5927042f64a81
uhyun-fe <feyouhyun0957@gmail.com>
2025-05-08 16:57:43 +0900

    #shopify - vite.config.js 수정 취소

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,7 @@ export default defineConfig(function ({ mode }) {
         resolve: {
             preserveSymlinks: true,
         },
-        css: {
-            devSourcemap: true,
-        },
         build: {
-            cssCodeSplit: true,
-            minify: 'esbuild',
-            sourcemap: false,
             chunkSizeWarningLimit: 1000,
             rollupOptions: {
                 output: {
